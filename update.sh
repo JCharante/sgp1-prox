46362e69cf58b94bc0cad521e82c5ef1a586e1ac
JCharante <git@jcharante.com>
2020-06-07 14:06:14 +0700

    add new service; add deploy scripts

---
+++ update.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t jcharante/sgp1-prox . && docker push jcharante/sgp1-prox && docker-machine ssh queen-1 'docker service update --with-registry-auth --image jcharante/sgp1-prox proxy'
